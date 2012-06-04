f0e33b59a01a6fe0b5b3f8b389ddfe9d0bdc0794
David A. Cuadrado <krawek@gmail.com>
2012-06-04 12:44:48 -0500

    allow to configure initial user from heroku settings
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(:email => "example@email.com", :password => "key")
+ENV['DEFAULT_EMAIL'] ||= "example@email.com"
+ENV['DEFAULT_PASSWORD'] ||= "key"
+ENV['DEFAULT_COUNTRY_CODE'] ||= "1"
+ENV['DEFAULT_CELLPHONE'] ||= "555-555-5555"
+
+user = User.where(:email => ENV['DEFAULT_EMAIL']).first
+if !user
+  user = User.new(:email => ENV['DEFAULT_EMAIL'], :password => ENV['DEFAULT_PASSWORD'])
+end
+
+authy = Authy::API.register_user(:email => ENV['DEFAULT_EMAIL'], :country_code => ENV['DEFAULT_COUNTRY_CODE'], :cellphone => ENV['DEFAULT_CELLPHONE'])
+user.authy_id = authy.id
+user.save
