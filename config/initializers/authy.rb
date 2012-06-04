17167bb6d39dc23ff722b77e01dd688ae2efccb9
David A. Cuadrado <krawek@gmail.com>
2012-06-04 11:56:28 -0500

    allow to configure your own api keys on heroku
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,4 +1,4 @@
 puts "Configuring authy"
 
-Authy.api_uri = "http://sandbox-api.authy.com"
-Authy.api_key = "772f064cfb3898cd29050768267bee1b" # rails example app on sandbox
+Authy.api_uri = ENV['AUTHY_API_URL'] || "http://sandbox-api.authy.com"
+Authy.api_key = ENV['AUTHY_API_KEY'] || "772f064cfb3898cd29050768267bee1b" # rails example app on sandbox
