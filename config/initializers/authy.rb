d50bb601eaba9a50e2bf559cdff3a204bddccba8
Johanna Mantilla Duque <johanna1431@gmail.com>
2014-07-22 15:36:03 -0500

    Update the sandbox API key
    The previous api key is not valid.

--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,4 +1,4 @@
 puts "Configuring authy"
 
 Authy.api_uri = ENV['AUTHY_API_URL'] || "http://sandbox-api.authy.com"
-Authy.api_key = ENV['AUTHY_API_KEY'] || "772f064cfb3898cd29050768267bee1b" # rails example app on sandbox
+Authy.api_key = ENV['AUTHY_API_KEY'] || "d57d919d11e6b221c9bf6f7c882028f" # rails example app on sandbox
